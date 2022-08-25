72e32dc79714ce872438d91da71e52454194e347
ArtemLeshch <artemleshchukov02@gmail.com>
2022-08-25 09:55:11 +0400

    2 spheres are created and rendering to start

--- raytracing.cpp
+++ raytracing.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 
 #if defined __linux__ || defined __APPLE__
 // Compiled for Linux
@@ -20,11 +22,70 @@ public:
 		this->y = y;
 		this->z = z;
 	}
+
+	Vec3& normalize()
+	{
+		T len = length();
+		if (len > 0) 
+		{
+			T invlen = 1 / len;
+			x *= invlen; 
+			y *= invlen, 
+			z *= invlen;
+		}
+		return *this;
+	}
+
+	T length2() const { return x * x + y * y + z * z; }
+	T length() const { return sqrt(length2()); }
 };
+
+typedef Vec3<float> Vec3f;
+
+
+class Sphere 
+{
+public:
+	Vec3f center;
+	float radius, radius2;
+	Vec3f surfaceColor, emissionColor;
+	float transparency, reflection;
+
+	Sphere(const Vec3f& c, const float& r, const Vec3f& sc, const float& transp = 0, const float& refl = 0, const Vec3f& ec = 0)
+		: center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec), transparency(transp), reflection(refl)
+	{ }
+
+};
+
+
+void render(const std::vector<Sphere>& spheres)
+{
+	unsigned int width = 640, height = 480;
+	Vec3f *image = new Vec3f[width * height], *pixel = image;
+	float invWidth = 1 / float(width), invHeight = 1 / float(height);
+	float fov = 30, aspectratio = width / float(height);
+	float angle = tan(M_PI * 0.5 * fov / 180);
+	// Trace rays
+	for (unsigned int y = 0; y < height; y++) {
+		for (unsigned int x = 0; x < width; x++, ++pixel) {
+			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+			float yy = (1 - 2 * ((x + 0.5) * invHeight)) * angle;
+			Vec3f raydir(xx, yy, -1);
+			raydir.normalize();
+
+		}
+	}
+}
 
 
 int main()
 {
-	Vec3<double> vect;
+	std::vector<Sphere> spheres;
+	spheres.emplace_back(Sphere(Vec3f(5.0, -1, -15), 2,	 Vec3f(0.90, 0.76, 0.46),	1, 0.0));
+
+	// light
+	spheres.emplace_back(Sphere(Vec3f(0.0, 20, -30), 3, Vec3f(0.0, 0.0, 0.0),		0, 0.0, Vec3f(3)));
+
+	render(spheres);
 	return 0;
 }
